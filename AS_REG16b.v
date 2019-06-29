67827831d90c8bae516151a929ba766c9c8ca3cb
Prog_PICs <30624560+ProgPICs@users.noreply.github.com>
2019-06-29 13:35:23 +0200

    Update AS_REG16b.v

--- AS_REG16b.v
+++ AS_REG16b.v
@@ -1,24 +1,6 @@
 // EEPROM for microcode logic
 // Antonio Sanchez
 
-`define HLT  15	//Halt
-`define MI   14	//Memory address IN
-`define RI   13	//RAM IN
-`define RO   12	//RAM OUT
-`define IO   11	//Instruction Register OUT
-`define II   10	//Instruction Register IN
-`define AI   9	//Register A IN
-`define AO   8	//Register A OUT
-`define EO   7	//SUM OUT
-`define SU   6	//ALU Substract
-`define BI   5	//Register B IN
-`define OI   4	//Display IN
-`define CE   3	//Program Counter Enable
-`define CO   2	//Program Counter OUT
-`define J    1	//Program Counter IN
-`define FI   0	//ALU Flags LOAD
-
-
 module as_reg16b
   (input [8:0]address,
    output [15:0]result
